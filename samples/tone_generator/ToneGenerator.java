39ffe560426d2a775a74af5d2940ab93e2db89e1
Michael Kohn <mike@mikekohn.net>
2016-03-25 12:52:44 -0500

    Adding new tone generator sample program.

---
+++ samples/tone_generator/ToneGenerator.java
@@ -0,0 +1,13 @@
+
+import net.mikekohn.java_grinder.W65C265SXB;
+
+public class ToneGenerator
+{
+  static public void main(String[] args)
+  {
+    W65C265SXB.controlTones(4000, 4000, true, true);
+
+    while(true);
+  }
+}
+
