82c691895e16af9e9c8b241217e87361decc5f0d
Michael Kohn <mike@mikekohn.net>
2016-03-25 05:27:22 -0500

    Adding 65c02 program and renaming original program to 65c816.

---
+++ samples/led_blink/led_blink_65c02.asm
@@ -0,0 +1,43 @@
+.org 0x1000
+
+start:
+
+  ;; The next three lines should be removed if running on a pure
+  ;; 6502 chip.  The sec / xce combination of 65C816 instructions
+  ;; put the chip in 6502 mode.
+.65816
+  ; set 6502 mode
+  sec
+  xce
+
+.65xx
+  ;; Turn off interrupts to protect from ROM routines running
+  sei
+
+  ;lda #0x00
+  ;sta 0xdf23
+
+main:
+  ;; LED off
+  lda #0xff
+  sta 0xdf23
+  jsr delay
+ 
+  ; LED on
+  lda #0x00
+  sta 0xdf23
+  jsr delay
+
+  jmp main
+
+delay:
+  ldy #0x00
+delay_outer:
+  ldx #0x00
+delay_inner:
+  dex
+  bne delay_inner
+  dey
+  bne delay_outer
+  rts
+
--- samples/led_blink/led_blink_65c816.asm
+++ samples/led_blink/led_blink_65c816.asm
@@ -2,14 +2,17 @@
 .org 0x1000
 
 start:
-  ; set native mode
+  ; Disable interrupts to protect from ROM routines running
+  sei
+
+  ; Set native mode
   clc
   xce
 
-  ; set A to 8-bit
+  ; Set A to 8-bit
   sep #0x20
 
-  ; set X/Y to 16-bit
+  ; Set X/Y to 16-bit
   rep #0x10
 
 main:
