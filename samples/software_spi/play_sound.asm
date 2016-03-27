26ec8125537fb9f141e4a4eb0c3bd2e612e58ac9
Michael Kohn <mike@mikekohn.net>
2016-03-27 06:11:00 -0500

    Changing software_spi.asm to be a generic program and play_sound.asm to be a sound player.

--- samples/software_spi/play_sound.asm
+++ samples/software_spi/play_sound.asm
@@ -1,7 +1,4 @@
 .org 0x1000
-  ;; These are some generic SPI routines.  To move to different ports or
-  ;; pins, the AND/OR instructions need to be changed and the output
-  ;; ports changed.
 
 start:
 
@@ -17,41 +14,77 @@ start:
   ;; Turn off interrupts to protect from ROM routines running
   sei
 
-  ;; Set bits 0,1,2 on Port 4 (P4X) as output
-  lda #0x07
-  ora 0xdf24
+  ;; Set entire Port 4 (P4X) as output
+  lda #0xff
   sta 0xdf24
 
   ;; P40: /CS = 1
   ;; P41: CLK = 0
   ;; P44: DATA = 0
   lda #0x01
-  ora 0xdf20
-  and #0xf9
   sta 0xdf20
 
-main:
+.if 0
+test:
   jsr spi_enable
-  lda #0x30
+  lda #0x3f
   jsr spi_write
   lda #0xff
   jsr spi_write
   jsr spi_disable
 while_1:
   jmp while_1
+.endif
+
+main:
+  jsr spi_enable
+low_byte:
+  lda data
+  cmp #0
+  beq main_sound_done
+  jsr spi_write
+high_byte:
+  lda data + 1
+  jsr spi_write
+  jsr spi_disable
+
+  ;; Increment pointer to low DAC byte
+  lda #2
+  clc
+  adc low_byte + 1
+  sta low_byte + 1
+  lda #0
+  adc low_byte + 2
+  sta low_byte + 2
+
+  ;; Increment pointer to high DAC byte
+  lda #2
+  clc
+  adc high_byte + 1
+  sta high_byte + 1
+  lda #0
+  adc high_byte + 2
+  sta high_byte + 2
+
+  ;; Delay
+  ldx #0x00
+delay:
+  dex
+  bne delay
+  jmp main
+
+main_sound_done:
+  jmp main_sound_done
 
 ;; /CS = 0
 spi_enable:
-  lda #0xf8
-  and 0xdf20
+  lda #0x00
   sta 0xdf20
   rts
 
 ;; /CS = 1
 spi_disable:
   lda #0x01
-  ora 0xdf20
-  and #0xf9
   sta 0xdf20
   rts
 
@@ -63,18 +96,14 @@ spi_write_next_bit:
   tay
   bcc spi_write_clock_0
   lda #0x04
-  ora 0xdf20
   sta 0xdf20
-  lda #0x02
-  ora 0xdf20
+  lda #0x06
   sta 0xdf20
   jmp spi_write_clock_off
 spi_write_clock_0:
-  lda #0xf8
-  and 0xdf20
+  lda #0x00
   sta 0xdf20
   lda #0x02
-  ora 0xdf20
   sta 0xdf20
 spi_write_clock_off:
   lda #0x04
