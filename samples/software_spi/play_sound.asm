0dd3ae0a0b966d02337077e63583f471f39526ef
Michael Kohn <mike@mikekohn.net>
2016-03-27 11:19:47 -0500

    inlining software SPI and removing as many instrutions as possible to increase speed.

--- samples/software_spi/play_sound.asm
+++ samples/software_spi/play_sound.asm
@@ -14,8 +14,8 @@ start:
   ;; Turn off interrupts to protect from ROM routines running
   sei
 
-  ;; Set entire Port 4 (P4X) as output
-  lda #0xff
+  ;; Set pins 0,1,2 of Port 4 (P4X) as output
+  lda #0x07
   sta 0xdf24
 
   ;; P40: /CS = 1
@@ -24,38 +24,57 @@ start:
   lda #0x01
   sta 0xdf20
 
-.if 0
-test:
-  jsr spi_enable
-  lda #0x3f
-  jsr spi_write
-  lda #0xff
-  jsr spi_write
-  jsr spi_disable
-while_1:
-  jmp while_1
-.endif
+  ; LED on
+  lda #0x00
+  sta 0xdf23
 
 main:
-  jsr spi_enable
-low_byte:
+  ;jsr spi_enable
+  lda #0x00
+  sta 0xdf20
+
+high_byte:
   lda data
   cmp #0
   beq main_sound_done
-  jsr spi_write
-high_byte:
-  lda data + 1
-  jsr spi_write
-  jsr spi_disable
 
-  ;; Increment pointer to low DAC byte
-  lda #2
-  clc
-  adc low_byte + 1
-  sta low_byte + 1
-  lda #0
-  adc low_byte + 2
-  sta low_byte + 2
+  ;; Write DAC high byte
+  ldx #8
+high_spi_write_next_bit:
+  rol
+  tay
+  lda #0x02
+  bcc low_clock_0
+  lda #0x06
+low_clock_0:
+  sta 0xdf20
+  lda #0x00
+  sta 0xdf20
+  tya
+  dex
+  bne high_spi_write_next_bit
+
+  ;; Write DAC low byte
+low_byte:
+  lda data + 1
+  ldx #8
+low_spi_write_next_bit:
+  rol
+  tay
+  lda #0x02
+  bcc high_clock_0
+  lda #0x06
+high_clock_0:
+  sta 0xdf20
+  lda #0x00
+  sta 0xdf20
+  tya
+  dex
+  bne low_spi_write_next_bit
+
+  ;jsr spi_disable
+  lda #0x01
+  sta 0xdf20
 
   ;; Increment pointer to high DAC byte
   lda #2
@@ -66,55 +85,39 @@ high_byte:
   adc high_byte + 2
   sta high_byte + 2
 
-  ;; Delay
-  ldx #0x00
-delay:
-  dex
-  bne delay
+  ;; Increment pointer to low DAC byte
+  lda #2
+  clc
+  adc low_byte + 1
+  sta low_byte + 1
+  lda #0
+  adc low_byte + 2
+  sta low_byte + 2
+
   jmp main
 
 main_sound_done:
-  jmp main_sound_done
+  ;; LED off
+  lda #0xff
+  sta 0xdf23
 
-;; /CS = 0
-spi_enable:
-  lda #0x00
-  sta 0xdf20
+  ;; Reset pointer to sound
+  lda #data & 0xff
+  sta high_byte + 1 
+  lda #data >> 8
+  sta high_byte + 2 
+  lda #(data + 1) & 0xff
+  sta low_byte + 1 
+  lda #(data + 1) >> 8
+  sta low_byte + 2 
+
   rts
+while_1:
+  jmp while_1
 
-;; /CS = 1
-spi_disable:
-  lda #0x01
-  sta 0xdf20
-  rts
-
-;; spi_write(A)
-spi_write:
-  ldx #8
-spi_write_next_bit:
-  rol
-  tay
-  bcc spi_write_clock_0
-  lda #0x04
-  sta 0xdf20
-  lda #0x06
-  sta 0xdf20
-  jmp spi_write_clock_off
-spi_write_clock_0:
-  lda #0x00
-  sta 0xdf20
-  lda #0x02
-  sta 0xdf20
-spi_write_clock_off:
-  lda #0x04
-  and 0xdf20
-  sta 0xdf20
-  tya
-  dex
-  bne spi_write_next_bit
-  rts
-
+.org 0x1200
 data:
-  db 0x30, 0xff, 0x00, 0x00
+  ;db 0x3f, 0xff, 0x30, 0x00, 0x00, 0x00
 ;.binfile "new.img"
+;.binfile "sound.img"
 
